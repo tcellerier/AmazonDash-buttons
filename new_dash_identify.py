8059024712001dc8eb49377ee96673fae771975a
Thomas Cellerier <tcellerier@users.noreply.github.com>
2016-12-28 00:24:03 +0100

    Update new_dash_identify.py

--- new_dash_identify.py
+++ new_dash_identify.py
@@ -9,7 +9,7 @@
 # sudo apt-get install tcpdump
 
 from scapy.all import *
-import time
+from datetime import datetime
 
 
 def button_detect(pkt):
@@ -18,7 +18,7 @@ def button_detect(pkt):
         return
 
     print ""
-    print time.strftime('%X ') + pkt.summary() # affiche l'heure et un résumé du paquet
+    print str(datetime.now()) + " " + pkt.summary() # affiche l'heure et un résumé du paquet
    
     print "New button: " + pkt[Ether].src
 
@@ -28,9 +28,10 @@ def button_detect(pkt):
 #  Bouton v2 : 2 DHCP requests from 0.0.0.0 first, followed by ARP requests for other network devices (e.g. the gateway) using the IP address they have been assigned
 #                NB : Les DHCP requests ne sont pas réémises si le bouton est repressé trop rapidement 
 def startSniff():
-	print sniff(prn=button_detect, filter="(arp or (udp and src port 68 and dst port 67)) and src host 0.0.0.0", store=0)
+    print "Sniffing started"
+    print sniff(prn=button_detect, filter="(arp or (udp and src port 68 and dst port 67)) and src host 0.0.0.0", store=0)
 
 
 
 if __name__ == '__main__':
-	startSniff()
+    startSniff()
