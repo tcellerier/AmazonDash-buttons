a508f64547c7a3142fb1c3c1fa51e6fcbe5937c0
Thomas Cellerier <tcellerier@users.noreply.github.com>
2016-12-28 00:23:39 +0100

    Update actions_dash.py

--- actions_dash.py
+++ actions_dash.py
@@ -6,7 +6,7 @@
 # sudo apt-get install tcpdump
 
 from scapy.all import * 
-import time
+from datetime import datetime
 import os
 
 
@@ -24,16 +24,17 @@ def button_detect(pkt):
     if not pkt.haslayer(Ether):
         return
 
-    min_delay = 2.5    # delai minimum en secondes pour la prise en compte d'un nouveau paquet
+    min_delay = 3    # delai minimum en secondes pour la prise en compte d'un nouveau paquet
+    now = datetime.now()
     mac = pkt[Ether].src
     if not (mac in lasttime):   # 1ere fois que le boutton est pressé
         delay_lastpush = 999
     else:
-        delay_lastpush = time.time() - lasttime[mac]
+        delay_lastpush = (now - lasttime[mac]).total_seconds() # nombre de secondes.microsecondes
 
-
+    print str(now)
     if delay_lastpush >= min_delay:
-        lasttime[mac] = time.time()
+        lasttime[mac] = now
         
         # actions des boutons
         if mac == buttons['but1']:
@@ -52,7 +53,7 @@ def startSniff():
     global lasttime
     lasttime = {}
 
-    sniff_filters = " or ".join(["ether host " + buttons[button] for button in buttons]) # filtre uniquement sur les adresses mac des boutons
+    sniff_filters = " or ".join(["ether src host " + buttons[button] for button in buttons]) # filtre uniquement sur les adresses mac des boutons
     print(sniff(prn=button_detect, filter=sniff_filters, store=0))
 
 
